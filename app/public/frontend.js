219f8d6017ffd819492bfd0745091a680028ed67
cgad <35946778+cgad@users.noreply.github.com>
2018-09-11 18:30:12 -0600

    Styling

--- app/public/frontend.js
+++ app/public/frontend.js
@@ -1,22 +1,17 @@
 $("#submit").on("click", function(event) {
     event.preventDefault();
 
+    var scores = [];
+    for (var i = 1; i < 11; i++) {
+        var score = $("input[name=q" + i + "]:checked").val();
+        scores.push(score);
+    };
+    console.log(scores);
+
     var newFriend = {
         name: $("#name").val().trim(),
         photo: $("#photo").val().trim(),
-        scores: [
-            // loop here like below?
-            $("input[name=q1]:checked").val(),
-            $("input[name=q2]:checked").val(),
-            $("input[name=q3]:checked").val(),
-            $("input[name=q4]:checked").val(),
-            $("input[name=q5]:checked").val(),
-            $("input[name=q6]:checked").val(),
-            $("input[name=q7]:checked").val(),
-            $("input[name=q8]:checked").val(),
-            $("input[name=q9]:checked").val(),
-            $("input[name=q10]:checked").val()
-        ]
+        scores: scores
     };
 
     $.post("/api/friends", newFriend, function(data) {
